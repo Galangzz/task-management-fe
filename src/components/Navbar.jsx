8326be1e90a4154df266ca95e84089d5ee30e5b5
galangsukamenolong <galangpro280605@gmail.com>
2025-11-05 13:13:27 +0700

    channge 'Tugas saya' in memmory

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar({ tabs, addList }) {
 
     return (
         <div className="NavBar min-w-screen flex items-center px-10 py-2 bg-(--background-header) ">
-            <ul className="flex py-1 gap-x-3 font-mono text-xl items-center w-max h-full snap-x snap-mandatory overflow-x-auto overflow-y-clip scrollbar-hide">
+            <ul className="flex py-1 px-2 gap-x-3 font-mono text-xl items-center w-max h-full snap-x snap-mandatory overflow-x-auto overflow-y-clip scrollbar-hide">
                 <li className="">
                     <Button
                         type={'button'}
@@ -19,21 +19,19 @@ function Navbar({ tabs, addList }) {
                         <FaStar className="h-full w-full flex " />
                     </Button>
                 </li>
-                <li className="">
-                    <Button
-                        type={'button'}
-                        className={'listTask'}
-                        onClick={() => navigate('/')}
-                    >
-                        Tugas Saya
-                    </Button>
-                </li>
+            
                 {tabs.map((tab) => (
                     <li key={tab.id}>
                         <Button
                             type={'button'}
                             className={'listTask'}
-                            onClick={() => navigate(`/${tab.id}`)}
+                            onClick={() => {
+                                if (tab.id === 'main-task') {
+                                    navigate('/');
+                                    return;
+                                }
+                                navigate(`/${tab.id}`);
+                            }}
                         >
                             {tab.title}
                         </Button>
