38904389ed2785afbc118443e9b4b63eb6f8aaf8
galangsukamenolong <galangpro280605@gmail.com>
2025-11-12 16:14:41 +0700

    feat: add hook time control

--- src/hooks/useTime.js
+++ src/hooks/useTime.js
@@ -1,3 +1,68 @@
-function useTime(){
-    
+import { useCallback, useRef, useState } from 'react';
+import useInput from './useInput';
+
+function useTime() {
+    const [title, setTitle] = useInput('');
+    const [detail, setDetail] = useInput('');
+    const [isOpenDetail, setIsOpenDetail] = useState(false);
+    const [isOpenCalendar, setIsOpenCalendar] = useState(false);
+    const [isOpenTime, setIsOpenTime] = useState(false);
+    const [isSubmitDateTime, setIsSubmitDateTime] = useState(false);
+    const [selected, setSelected] = useState(new Date());
+    // const [time, setTime] = useState(new Date());
+    // console.log('Time: ',selected)
+
+    const textRef = useRef(null);
+
+    const handleInputDetail = useCallback(
+        (e) => {
+            const el = textRef.current;
+            el.style.height = 'auto';
+            el.style.height = `${el.scrollHeight}px`;
+            setDetail(e);
+        },
+        [setDetail]
+    );
+
+    const handleAddDetail = useCallback(() => {
+        setIsOpenDetail(true);
+    }, []);
+
+    const handleOpenCalendar = useCallback(() => {
+        setIsOpenCalendar(true);
+    }, []);
+
+    const handleSubmitDateTime = useCallback(() => {
+        setIsOpenCalendar(false);
+
+        setIsSubmitDateTime(true);
+    }, []);
+
+    // const handleTimeChange = useCallback((newTime) => {
+    //     setTime(newTime);
+    // }, []);
+
+    return [
+        title,
+        detail,
+        isOpenDetail,
+        isOpenCalendar,
+        isOpenTime,
+        isSubmitDateTime,
+        selected,
+        // time,
+        textRef,
+        setTitle,
+        setIsSubmitDateTime,
+        handleInputDetail,
+        handleAddDetail,
+        handleOpenCalendar,
+        setIsOpenCalendar,
+        setIsOpenTime,
+        handleSubmitDateTime,
+        setSelected,
+        // handleTimeChange,
+    ];
 }
+
+export default useTime;
